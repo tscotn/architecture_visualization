605c98777728a263c175159c206de4cdb63bb140
Scot Nielson <44238650+tscotn@users.noreply.github.com>
2020-07-05 17:37:35 -0600

    place js script in separate file

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,20 @@
-div#sink {
-  width: 52.5vw;
-  height: 70vw;
-  max-width: 300px;
-  max-height: 400px;
-  overflow: hidden; }
-div#sink figure {
-  background-image: url('../assets/images/sink.jpg');
-  background-size: cover;
-  position: relative;
-  font-size: 0;
-  width: 100%;
-  height: 100%;
-  margin: 0;
-}
-div#sink figure > img {
-  position: relative;
-  width: 100%;
-}
-div#sink figure div {
-  background-image: url('../assets/images/sink_after.jpg');
-  background-size: cover;
-  position: absolute;
-  width: 100%;
-  box-shadow: 0 5px 10px -2px rgba(0,0,0,0.3);
-  overflow: hidden;
-  bottom: 0;
-  height: 100%;
+//sink slider function
+var sink_divisor = document.getElementById("sink_divisor"),
+  sink_slider = document.getElementById("sink_slider");
+  function moveSink_Divisor() {
+    sink_divisor.style.width = sink_slider.value+"%";
+  }
+
+//island slider function
+var island_divisor = document.getElementById("island_divisor"),
+  island_slider = document.getElementById("island_slider");
+  function moveIsland_Divisor() {
+    island_divisor.style.width = island_slider.value+"%";
 }
 
-input[type=range]{
-  -webkit-appearance:none;
-  -moz-appearance:none;
-  position: relative;
-  top: -2rem; left: -2%;
-  background-color: rgba(255,255,255,0.1);
-  width: 102%;
+//oven slider function
+var oven_divisor = document.getElementById("oven_divisor"),
+  oven_slider = document.getElementById("oven_slider");
+  function moveOven_Divisor() {
+    oven_divisor.style.width = oven_slider.value+"%";
 }
-input[type=range]:focus {
-  outline: none;
-}
-input[type=range]:active {
-  outline: none;
-}
-
-input[type=range]::-moz-range-track {
-  -moz-appearance:none;
-    height:15px;
-    width: 98%;
-    background-color: rgba(255,255,255,0.1);
-    position: relative;
-    outline: none;
- }
-input[type=range]::active {
-  border: none;
-  outline: none;
-}
-input[type=range]::-webkit-slider-thumb {
-    -webkit-appearance:none;
-    width: 20px; height: 15px;
-    background: #fff;
-    border-radius: 0;
-   }
-input[type=range]::-moz-range-thumb {
-  -moz-appearance: none;
-  width: 20px;
-  height: 15px;
-  background: #fff;
-  border-radius: 0;
-     }
-input[type=range]:focus::-webkit-slider-thumb {
-    background: rgba(255,255,255,0.5);
-   }
-input[type=range]:focus::-moz-range-thumb {
-    background: rgba(255,255,255,0.5);
-   }
